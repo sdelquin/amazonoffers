93307a80a39e68c6fbd0c3fecb7e22ab7556df9d
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-02-20 10:55:24 +0000

    Remove unused settings

--- settings.py
+++ settings.py
@@ -5,11 +5,6 @@ from prettyconf import config
 PROJECT_DIR = Path(__file__).parent
 PROJECT_NAME = PROJECT_DIR.name
 
-TARGET_URL = config('TARGET_URL', default='')
-USER_AGENT = config(
-    'USER_AGENT',
-    default='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-)
 CONFIG_PATH = config('CONFIG_PATH', default=PROJECT_DIR / 'config.yaml', cast=Path)
 STORAGE_PATH = config('STORAGE_PATH', default='deliveries.dbm')
 
